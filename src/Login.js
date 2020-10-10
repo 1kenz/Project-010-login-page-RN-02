96b95a6882f806346b784978c1957c5e231eb2a8
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-10 18:53:25 +0300

    update

---
+++ src/Login.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { SafeAreaView, View, Text, Image, StyleSheet, Dimensions } from 'react-native';
+
+import { Input, Button } from './components'
+
+const Login = () => {
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#b2dfdb' }}>
+            <View style={{ flex: 1 }}>
+                <Image
+                    source={require('./assets/cart.png')}
+                    style={styles.logoStyle}
+                />
+
+                <Input holder="E-posta adresinizi giriniz.." />
+                <Input holder="Şifrenizi giriniz.." />
+
+                <Button text="Giriş Yap" />
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default Login
+
+const styles = StyleSheet.create({
+    logoStyle: {
+        resizeMode: 'contain',
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height * 0.40
+    }
+})
